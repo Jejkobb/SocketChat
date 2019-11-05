bc29a3e4c0d2b2f83cd77d539adcda76cc7f4c77
Jakob Hanses <hansesjakob@gmail.com>
2019-11-05 15:35:18 +0200

    first commit

---
+++ public/app.js
@@ -0,0 +1,30 @@
+var inp = document.getElementById('input-box');
+var out = document.getElementById('log');
+var socket;
+
+socket = io.connect('http://localhost:5000/');
+
+function drawMsg(message){
+  var p = document.createElement('p');
+  var pAmt = document.getElementsByClassName('message').length;
+  if((pAmt % 2) == 0){
+    p.style.backgroundColor = "#444";
+  }
+  p.innerHTML = message;
+  p.className = "message";
+  out.prepend(p);
+}
+
+inp.addEventListener("keyup", function(e) {
+    e.preventDefault();
+    if (e.keyCode === 13) {
+        socket.emit('chat message', this.value);
+        this.value = "";
+    }
+});
+
+socket.on('chat message', newMessage);
+
+function newMessage(data){
+  drawMsg(data);
+}
--- server.js
+++ server.js
@@ -1,8 +1,25 @@
 var express = require('express');
 
 var app = express();
-var server = app.listen(3000);
+var server = app.listen(5000);
 
 app.use(express.static('public'));
 
 console.log("My socket server is running!");
+
+var socket = require('socket.io');
+
+var io = socket(server);
+
+io.on('connection', function(socket){
+  console.log(socket.id + ' connected');
+
+  socket.on('disconnect', function(){
+    console.log(socket.id + ' disconnected');
+  });
+
+  socket.on('chat message', function(msg){
+    console.log('message: ' + msg);
+    io.emit('chat message', msg);
+  });
+});
